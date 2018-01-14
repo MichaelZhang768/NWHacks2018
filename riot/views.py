5be686004ce593e6414557b4ed468590c8603040
Carol Ng <ng.km.carol@gmail.com>
2018-01-13 21:24:02 -0800

    endpoint good

--- riot/views.py
+++ riot/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 from riot import config
 from riot.models import Champion
@@ -9,12 +10,27 @@ def index(request):
     return HttpResponse("Hello, world. You're at the Riot index.")
 
 def get_palette(request):
+    results = {}
     dpm = DataProcessingManager()
     champ_name = request.GET.get('champion_name')
 
-    for k, v in config.CHAMP_ID_TO_NAME.items():
-        champ_id = k
-        if v.lower() == champ_name.lower():
-            break
+    try:
+        if not champ_name:
+            raise ValueError("Query param 'champion_name' required")
 
-    return HttpResponse(dpm.get_palette_from_champion(champ_id=champ_id))
+        champ_id = None
+        for k, v in config.CHAMP_ID_TO_NAME.items():
+            if v.lower() == champ_name.lower():
+                champ_id = k
+                break
+
+        if not champ_id:
+            raise ValueError("Champion name: %s is not a valid name" % champ_name)
+
+        palette = dpm.get_palette_from_champion(champ_id=champ_id)
+
+        results['results'] = palette.split()
+    except ValueError as e:
+        results['error'] = str(e)
+
+    return HttpResponse(json.dumps(results))
