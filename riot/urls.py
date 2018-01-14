08fb796b1a9c83f13b2ee3462f3d02a0ed1f52d0
Carol Ng <ng.km.carol@gmail.com>
2018-01-13 21:02:47 -0800

    add search for palette by name

--- riot/urls.py
+++ riot/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('get_palette', views.get_palette, name='get_palette') 
 ]
--- riot/views.py
+++ riot/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from riot import config
+from riot.models import Champion
+from riot.data import DataProcessingManager
 
 def index(request):
     return HttpResponse("Hello, world. You're at the Riot index.")
 
+def get_palette(request):
+    dpm = DataProcessingManager()
+    champ_name = request.GET.get('champion_name')
+
+    for k, v in config.CHAMP_ID_TO_NAME.items():
+        champ_id = k
+        if v.lower() == champ_name.lower():
+            break
+
+    return HttpResponse(dpm.get_palette_from_champion(champ_id=champ_id))
