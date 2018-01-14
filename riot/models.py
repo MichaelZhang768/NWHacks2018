1f0ad56f178c2b4c2ada5a5fc9536162469dbf3a
Michael Zhang <michaelzhang768@gmail.com>
2018-01-13 16:15:32 -0800

    Fixed names

--- riot/models.py
+++ riot/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Summoner(models.Model):
-    id = models.IntegerField(unique=True, null=False)
+    summoner_id = models.IntegerField(unique=True, null=False)
     name = models.CharField(unique=True, null=False, max_length=32)
 
 class Match(models.Model):
     result = models.BooleanField(null=False)
-    gameId = models.IntegerField(unique=True, null=False)
-    champId = models.IntegerField(unique=True, null=False)
-    summonerId = models.IntegerField(unique=True, null=False)
+    game_id = models.IntegerField(unique=True, null=False)
+    champ_id = models.IntegerField(unique=True, null=False)
+    summoner_id = models.IntegerField(unique=True, null=False)
 
