cdc8fbb154106eb06731a295d1d9b098815e36a7
Michael <michaelzhang768@gmail.com>
2018-01-13 21:39:26 -0800

    Extracted data with Riot API and added feature to input data into DB

--- riot/models.py
+++ riot/models.py
@@ -2,14 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Summoner(models.Model):
-    summoner_id = models.IntegerField(unique=True, null=False)
+    account_id = models.IntegerField(unique=True, null=False)
     name = models.CharField(unique=True, null=False, max_length=32)
 
 class Match(models.Model):
+    timestamp = models.DateField(null=True)
     result = models.BooleanField(null=False)
-    game_id = models.IntegerField(unique=True, null=False)
-    champ_id = models.IntegerField(unique=True, null=False)
-    summoner_id = models.IntegerField(unique=True, null=False)
+    game_id = models.BigIntegerField(null=False)
+    champ_id = models.IntegerField(null=False)
+    account_id = models.IntegerField(null=False)
+    
 
 class Champion(models.Model):
     champion_id = models.IntegerField(unique=True, null=False)
--- riot/riot.py
+++ riot/riot.py
@@ -1,6 +1,9 @@
-from secrets import secrets
+from riot import secrets
 from copy import deepcopy
+from riot.models import Summoner, Match
 import requests
+import time
+import datetime
 
 from riot import config
 
@@ -10,7 +13,7 @@ class RiotManager(object):
         self.current_secret_idx = 0
         self.current_secret = self.keys[self.current_secret_idx]
 
-    def get_params_with_auth(self, param):
+    def get_params_with_auth(self, param={}):
         dup_param = deepcopy(param)
         dup_param['api_key'] = self.get_current_secret()
         return dup_param
@@ -20,8 +23,66 @@ class RiotManager(object):
         if not skip_incr:
             self.current_secret_idx = (self.current_secret_idx + 1) % len(self.keys)
             self.current_secret = self.keys[self.current_secret_idx]
+            print(self.current_secret)
         return return_val
 
-    def get_matches(self, account_id, params):
+    def get_summoner_data(self, name):
+        url = '%ssummoner/v3/summoners/by-name/%s' % (config.RIOT_API_ROOT, name)
+        return requests.get(url, params=self.get_params_with_auth()).json()
+
+    def get_matches(self, account_id):
         url = '%smatch/v3/matchlists/by-account/%d' % (config.RIOT_API_ROOT, account_id)
-        return requests.get(url, params=self.get_params_with_auth(params))
+        return requests.get(url, params=self.get_params_with_auth()).json()
+
+    def get_match(self, match_id):
+        url = '%smatch/v3/matches/%d' % (config.RIOT_API_ROOT, match_id)
+        return requests.get(url, params=self.get_params_with_auth()).json()
+
+    def get_won_match(self, account_id, match):
+        participant_id = ""
+        participants = match['participantIdentities']
+        for participant in participants:
+            if(participant['player']['accountId'] == account_id):
+                participant_id = participant['participantId']
+                break;
+
+        participants_data = match['participants']
+        team_id = 0;
+        for participant_data in participants_data:
+            if(participant_data['participantId'] == participant_id):
+                team_id = participant_data['teamId']
+                break;
+
+        if(team_id == 100):
+            return match['teams'][0]['win'] == 'Win'
+        else:
+            return match['teams'][1]['win'] == 'Win'
+
+
+    def store_summoner_data(self, name):
+        rm = RiotManager()
+        summoner_data = rm.get_summoner_data(name)
+        account_id = summoner_data['accountId']
+
+        Summoner.objects.get_or_create(account_id=account_id, name=name)
+
+        matchDicts = []
+        matches = rm.get_matches(account_id)
+
+        for match in matches['matches']:
+            matchDicts.append({'gameId' : match['gameId'],
+                               'matchData' : rm.get_match(match['gameId']),
+                               'champion' : match['champion'],
+                               'timestamp' : match['timestamp']})
+
+        for matchDict in matchDicts:
+            try:
+                result=rm.get_won_match(account_id, matchDict['matchData'])
+                Match.objects.create(timestamp=datetime.datetime.fromtimestamp(matchDict['timestamp']/1000.0),
+                                     result=result, game_id=matchDict['gameId'],
+                                     champ_id=matchDict['champion'], account_id=account_id)
+            except Exception as e:
+                print(e)
+            
+
+        
