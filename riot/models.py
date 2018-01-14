8f0e9c557ba4a7115ccbc1ac6a3b2619c7d07c15
Paul Wang <paul_wang347@hotmail.com>
2018-01-14 02:20:16 -0800

    Fix migrations

--- riot/models.py
+++ riot/models.py
@@ -11,10 +11,8 @@ class Match(models.Model):
     game_id = models.BigIntegerField(null=False)
     champ_id = models.IntegerField(null=False)
     account_id = models.IntegerField(null=False)
-    
 
 class Champion(models.Model):
     champion_id = models.IntegerField(unique=True, null=False)
     name = models.CharField(unique=True, null=False, max_length=12)
     palette = models.CharField(null=False, max_length=64)
-
